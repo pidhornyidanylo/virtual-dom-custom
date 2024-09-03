837e740b9bed6b96f29e38fbc691db3b4e5f6a9c
Danylo Pidhornyi <105637978+pidhornyidanylo@users.noreply.github.com>
2024-09-03 15:38:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,162 @@
+const CREATE = 'CREATE'
+const REMOVE = 'REMOVE'
+const REPLACE = 'REPLACE'
+const UPDATE = 'UPDATE'
+const SET_PROP = 'SET_PROP'
+const REMOVE_PROP = 'REMOVE PROP'
+
+//// DIFF
+
+function changed(node1, node2) {
+    return typeof node1 !== typeof node2 || 
+    typeof node1 === "string" && node1 !== node2 || 
+    node1.type !== node2.type
+}
+
+function diffProps(newNode, oldNode) {
+    const patches = [];
+    const props = Object.assign({}, newNode.props, oldNode.props)
+    Object.keys(props).forEach(name => {
+        const newVal = newNode.props[name]
+        const oldValue = oldNode.props[name]
+        if(!newVal) {
+            patches.push({ type: REMOVE_PROP, name, value: oldNode })
+        } else if(!oldValue || newVal !== oldValue) {
+            patches.push({ type: SET_PROP, name, value: newVal })
+        }
+    })
+    return patches;
+}
+
+function diffChildren(newNode, oldNode) {
+    const patches = [];
+    const patchesLength = Math.max(
+        newNode.children.length,
+        oldNode.children.length
+    )
+    for (let i = 0; i < patchesLength; i++) {
+        patches[i] = diff(
+            newNode.children[i],
+            oldNode.children[i]
+        )
+    }
+    return patches
+}
+
+function diff(newNode, oldNode) {
+    if(!oldNode) {
+        return { type: CREATE, newNode }
+    }
+    if(!newNode) {
+        return { type: REMOVE }
+    }
+    if(changed(newNode, oldNode)) {
+        return { type: REPLACE, newNode }
+    }
+    if(newNode.type) {
+        return { type: UPDATE, children: diffChildren(newNode, oldNode), props: diffProps(newNode, oldNode) }
+    }
+}
+
+//// PATCH
+
+function createElement(node) {
+    if(typeof node === "string") {
+        return document.createTextNode(node)
+    }
+    const el = document.createElement(node.type);
+    setProps(el, node.props);
+    node.children.map(createElement).forEach(el.appendChild.bind(el))
+    return el;
+}
+
+function setProp(target, name, value) {
+    if(name === "className") {
+        return target.setAttribute("class", value);
+    }
+    target.setAttribute(name, value)
+}
+
+function setProps(target, props) {
+    Object.keys(props).forEach(name => {
+        setProp(target, name, props[name])
+    })
+}
+
+function removeProp(target, name) {
+    if(name === "className") {
+        return target.removeAttribute("class");
+    }
+    target.removeAttribute(name)
+}
+
+function patchProps(parent, patches) {
+    for(let i = 0; i < patches.length; i ++) {
+        const propsPatch = patches[i]
+        const { type, name, value } = propsPatch;
+        if(type === SET_PROP) {
+            setProp(parent, name, value)
+        }
+        if(type === REMOVE_PROP) {
+            removeProp(parent, name, value)
+        }
+    }
+}
+
+function patch(parent, patches, index = 0) {
+    if(!patches) { return }
+    const el = parent.childNodes[index]
+    switch (patches.type) {
+        case CREATE: {  
+            const { newNode } = patches
+            const newEl = createElement(newNode)
+            return parent.appendChild(newEl)
+        }
+        case REMOVE: {
+            return parent.removeChild(el)
+        }
+        case REPLACE: {
+            const { newNode } = patches
+            const newEl = createElement(newNode)
+            return parent.replaceChild(newEl, el)
+        }
+        case UPDATE: {
+            const { children, props } = patches;
+            patchProps(el, props)
+            for(let i = 0; i < children.length; i ++) {
+                patch(el, children[i], i)
+            }
+        }
+    }
+}
+
+//// My Application
+
+function flatten(arr) {
+    return [].concat.apply([], arr);
+}
+
+function h(type, props, ...children) {
+    props = props || {};
+    return { type, props, children: flatten(children) }
+}
+
+function view(count) {
+    const r = [...Array(count).keys()];
+    return <ul id="cool" className={`my-class-${count % 3}`}>
+        { r.map(n => <li>item { (count * n).toString() }</li>) }
+    </ul>
+}
+
+function tick(el, count) {
+    const patches = diff(view(count + 1), view(count));
+    patch(el,patches);
+    console.log(count, patches)
+    if(count > 20) { return }
+    setTimeout(() => tick(el, count + 1), 500)
+}
+
+function render(el) {
+    el.appendChild(createElement(view(0)));
+    setTimeout(() => tick(el, 0), 500)
+}
